5cd0de57e1baa20df940404edffe56129e7af2d0
Evon <fumblies@gmail.com>
2016-07-15 15:56:45 +0800

    manual settle conflicts

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD
-=======
 
->>>>>>> ce887647390ce92a059d845802a7dbf1d42027a6
 const bananasController = require('../controller/bananas_controller')
 const applesController = require('../controller/apples_controller')
 const cheesesController = require('../controller/cheeses_controller')
 const orangesController = require('../controller/oranges_controller')
 const pearsController = require('../controller/pears_controller')
 
-<<<<<<< HEAD
-=======
 router.route('/apples')
   // '/apples' GET
   .get(applesController.getAll)
@@ -21,7 +16,6 @@ router.route('/bananas')
   // '/bananas' GET
   .get(bananasController.getAll)
   .post(bananasController.createBanana)
->>>>>>> ce887647390ce92a059d845802a7dbf1d42027a6
 
 router.route('/apples')
   // '/apples' GET
@@ -35,23 +29,21 @@ router.route('/cheeses')
   // '/cheeses' GET
   .get(cheesesController.getAll)
   .post(cheesesController.createCheese)
-<<<<<<< HEAD
-  router.route('/pears')
-    // '/pears' GET
-    .get(pearsController.getAll)
-    .post(pearsController.createPear)
+
+router.route('/pears')
+  // '/pears' GET
+  .get(pearsController.getAll)
+  .post(pearsController.createPear)
 router.route('/oranges')
   // '/oranges' GET
   .get(orangesController.getAll)
   .post(orangesController.createOrange)
-=======
 
 router.route('/oranges')
   // '/apples' GET
   .get(orangesController.getAll)
   .post(orangesController.createOrange)
 
->>>>>>> ce887647390ce92a059d845802a7dbf1d42027a6
 router.route('/oranges/:id')
   // 'oranges/:id' GET
   .get(orangesController.getOrange)
@@ -59,8 +51,5 @@ router.route('/oranges/:id')
   .put(orangesController.updateOrange)
   // '/oranges/:id' DELETE
   .delete(orangesController.destroyOrange)
-<<<<<<< HEAD
-=======
 
->>>>>>> ce887647390ce92a059d845802a7dbf1d42027a6
 module.exports = router
