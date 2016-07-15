fe0511125d77f06b8d8d41bd2da77e085caef843
Winston <winstonmichmi@gmail.com>
2016-07-15 13:46:40 +0800

    add cheeses

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const Apple = require('../models/apple')
+const Cheese = require('../models/cheese')
 
 const applesController = require('../controller/apples_controller')
-
+const cheesesController = require('../controller/cheeses_controller')
 
 router.route('/apples')
 // '/apples' GET
 .get(applesController.getAll)
 .post(applesController.createApple)
 
-
+router.route('/cheeses')
+// '/cheeses' GET
+.get(cheesesController.getAll)
+.post(cheesesController.createCheese)
 
 // router.route('/apples/:id')
 // // '/apples/:id' GET
